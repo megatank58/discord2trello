c605261846a47082acf3ef7c15fb30667a468b5e
Megatank58 <tanumegatank@gmail.com>
2023-04-28 07:58:33 +0000

    chore: init

--- src/index.js
+++ src/index.js
@@ -2,10 +2,8 @@ import { Client, GatewayIntentBits } from "discord.js";
 import TrelloEvents from "trello-events";
 import "dotenv/config";
 
-// Create a client to emit relevant events.
 const client = new Client({ intents: GatewayIntentBits.Guilds });
 
-// Listen for the ready event
 client.once("ready", () => console.log("Ready!"));
 
 const trelloEvents = new TrelloEvents({
@@ -18,25 +16,22 @@ const trelloEvents = new TrelloEvents({
 
 setTimeout(() => {
   trelloEvents.on("addMemberToCard", async (event) => {
-    let guild = client.guilds.cache.get(process.env.discordToken);
-    let user = await guild.members.fetch({ query: event.data.member.name, force: true });
+    let guild = client.guilds.cache.get(process.env.guildId);
+    let user = (await guild.members.fetch({ query: event.data.member.name.toLowerCase(), force: true })).first();
 
     let role = guild.roles.cache.find(role => role.name === event.data.card.name);
-
-    user.roles.add(role);
+    await user.roles.add(role);
   });
 
   trelloEvents.on("removeMemberFromCard", async (event) => {
-    let guild = client.guilds.cache.get(process.env.discordToken);
-    let user = await guild.members.fetch({ query: event.data.member.name, force: true });
+    let guild = client.guilds.cache.get(process.env.guildId);
+    let user = (await guild.members.fetch({ query: event.data.member.name.toLowerCase(), force: true })).first();
 
     let role = guild.roles.cache.find(role => role.name === event.data.card.name);
-
-    user.roles.remove(role);
+    await user.roles.remove(role);
   });
 }, 5_000);
 
 trelloEvents.start(3_000);
 
-// Start the WebSocket connection.
 client.login(process.env.discordToken);
