81baf67071ea48b9a70075f281b238b2cb825310
Megatank58 <tanumegatank@gmail.com>
2023-04-29 06:49:00 +0000

    chore: init

--- src/index.js
+++ src/index.js
@@ -15,20 +15,24 @@ const trelloEvents = new TrelloEvents({
 });
 
 setTimeout(() => {
-  trelloEvents.on("addMemberToCard", async (event) => {
-    let guild = client.guilds.cache.get(process.env.guildId);
-    let user = (await guild.members.fetch({ query: event.data.member.name.toLowerCase(), force: true })).first();
+  let guild = client.guilds.cache.get(process.env.guildId);
 
-    let role = guild.roles.cache.find(role => role.name === event.data.card.name);
-    await user.roles.add(role);
-  });
+  trelloEvents.on("updateCard", async (event) => {
+    let username = event.data.card.name.split("/")[1].split("#")[0].trim();
+    let label = event.data.card.name.split("/")[2].trim();
 
-  trelloEvents.on("removeMemberFromCard", async (event) => {
-    let guild = client.guilds.cache.get(process.env.guildId);
-    let user = (await guild.members.fetch({ query: event.data.member.name.toLowerCase(), force: true })).first();
+    let user = (
+      await guild.members.fetch({
+        query: username,
+        force: true,
+      })
+    ).first();
 
-    let role = guild.roles.cache.find(role => role.name === event.data.card.name);
-    await user.roles.remove(role);
+    let role = guild.roles.cache.find((role) => role.name === label);
+
+    if (event.data.listAfter && event.data.listAfter.name == "Discontinued")
+      await user.roles.remove(role);
+    else await user.roles.add(role);
   });
 }, 5_000);
 
